66b2df3f1f17dfddb6e3bd538f00e2de567b8c85
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-08 17:11:17 +0530

    Changes with Own App.js and Index.js - Toggle Bio Option

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = { displayBio: false };
+        // this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My Name is Sajan. I am Software Developer</p>
+                <p>I am looking forward to work on React JS</p>
+                {this.state.displayBio ? (
+                    <div>
+                        <p>I live in Bangalore</p>
+                        <p>My favorite language is Javascript</p>
+                        <button onClick={this.toggleDisplayBio.bind(this)}>Show Less</button>
+                    </div>
+                ) : (
+                    <button onClick={this.toggleDisplayBio.bind(this)}>Read more</button>
+                )}
+            </div>
+        )
+    }
 }
 
 export default App;
