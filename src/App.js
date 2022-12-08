8015df57276480eb5f9b273a918533422839fddb
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-08 19:41:00 +0530

    Added Projects and Social Profiles Sections

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import profile from "./assets/profile.jpg";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = { displayBio: false };
-        // this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
-    }
+    state = { displayBio: false };
 
-    toggleDisplayBio() {
+    toggleDisplayBio = () => {
         this.setState({ displayBio: !this.state.displayBio });
     }
 
     render() {
         return (
             <div>
+                <img src={profile} alt='profile' className="profile" />
                 <h1>Hello!</h1>
                 <p>My Name is Sajan. I am Software Developer</p>
                 <p>I am looking forward to work on React JS</p>
@@ -21,11 +21,15 @@ class App extends Component {
                     <div>
                         <p>I live in Bangalore</p>
                         <p>My favorite language is Javascript</p>
-                        <button onClick={this.toggleDisplayBio.bind(this)}>Show Less</button>
+                        <button onClick={this.toggleDisplayBio}>Show Less</button>
                     </div>
                 ) : (
-                    <button onClick={this.toggleDisplayBio.bind(this)}>Read more</button>
+                    <button onClick={this.toggleDisplayBio}>Read more</button>
                 )}
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
             </div>
         )
     }
