1b471bc429b7ee649fee3feefdddcebe71325fd1
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-08 19:40:26 +0530

    Added Projects Section

---
+++ src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Example React Application',
+        description: 'A React App that I build, involving JS and core web dev concepts!',
+        link: 'https://github.com/SajanJainSJ/shuffle-and-sort-coding-exercise',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'Shuffle and Sort Application',
+        description: 'This project contains a page which consists of 9 numbered cards which can be manipulated by using the shuffle and sort buttons!',
+        link: 'https://github.com/SajanJainSJ/shuffle-and-sort-coding-exercise',
+        image: project2
+    },
+    {
+        id: 3,
+        title: 'Portfolio Application',
+        description: 'A React App for Portfolio of My Projects!',
+        link: 'https://github.com/SajanJainSJ/portfolio',
+        image: project3
+    }
+];
+
+export default PROJECTS;
