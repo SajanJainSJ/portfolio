42c3a5be41733adfcbe3cf11c479ca2a80c49e0c
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-14 13:54:21 +0530

    Refactor to Stateless Functional Components

--- src/components/SocialProfiles.js
+++ src/components/SocialProfiles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SOCIAL_PROFILES from '../data/socialProfiles';
 
-const SocialProfile = props => {
+const SocialProfile = (props) => {
     const { link, image } = props.socialProfile;
 
     return (
@@ -18,9 +18,9 @@ const SocialProfiles = () => (
         <h2>Connect with me!</h2>
         <div>
             {
-                SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-                    return <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE} />;
-                })
+                SOCIAL_PROFILES.map(SOCIAL_PROFILE => (
+                    <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE} />
+                ))
             }
         </div>
     </div>
