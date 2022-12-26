dd9d1a1ef52cca5ac2ac9e2aef26a6a6c6b79abe
Sajan Jain <sajan.jain@ocean.ibm.com>
2022-12-26 18:56:22 +0530

    Added Music Master Project to the Porfolio with Link

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,24 +5,24 @@ import project3 from "../assets/project3.png";
 const PROJECTS = [
     {
         id: 1,
+        title: 'Portfolio Application',
+        description: 'A React App for Portfolio of all my Projects!',
+        link: 'https://github.com/SajanJainSJ/portfolio',
+        image: project3
+    },
+    {
+        id: 2,
         title: 'Example React Application',
         description: 'A React App that I build, involving JS and core web dev concepts!',
         link: 'https://github.com/SajanJainSJ/shuffle-and-sort-coding-exercise',
         image: project1
     },
     {
-        id: 2,
-        title: 'Shuffle and Sort Application',
-        description: 'This project contains a page which consists of 9 numbered cards which can be manipulated by using the shuffle and sort buttons!',
-        link: 'https://github.com/SajanJainSJ/shuffle-and-sort-coding-exercise',
-        image: project2
-    },
-    {
         id: 3,
-        title: 'Portfolio Application',
-        description: 'A React App for Portfolio of My Projects!',
-        link: 'https://github.com/SajanJainSJ/portfolio',
-        image: project3
+        title: 'Music Master Application',
+        description: 'This is a React project where you can Search an Artist and the preview the tracks.',
+        link: 'https://github.com/SajanJainSJ/music-master',
+        image: project2
     }
 ];
 
